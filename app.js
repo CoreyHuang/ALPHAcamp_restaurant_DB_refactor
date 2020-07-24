bff172b960bebcafc25b5bc6791a04756285b15a
Corey <simon8888xsimon8888x@gmail.com>
2020-07-25 03:50:44 +0800

    feature: install mongoose and load default setting

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ app.listen(host, () => {
   console.log("Enable server...")
 })
 
+// 載入mongoose與相關設定
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true }) 
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => { console.log('mongodb error!') }) //連線失敗
+db.once('open', () => { console.log('mongodb connected!') }) //連線成功
+
+
+
+
 //Get method request---------------------------------------------------------------------------
 app.get('/', (req, res) => {
   res.render('index', { restaurants })
