b0124e10b94451c53be8e06a21684b25083824d7
Corey <simon8888xsimon8888x@gmail.com>
2020-07-25 21:24:26 +0800

    feature: add delete function

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const host = 3000
 let newDataError = 0
 
 //Require restaurant json file
-// const restaurants = require('./restaurant.json').results
 const restaurantSeed = require('./models/restaurant.js')
 
 //Set view engine
@@ -32,7 +31,6 @@ const db = mongoose.connection // 取得資料庫連線狀態
 db.on('error', () => { console.log('mongodb error!') }) //連線失敗
 db.once('open', () => { console.log('mongodb connected!') }) //連線成功
 
-
 //Get method request---------------------------------------------------------------------------
 app.get('/', (req, res) => {
   console.log("req.params.edit", req.params)
@@ -40,7 +38,6 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
-
 
 app.get('/restaurants/new', (req, res) => {
   res.render('new', { alert: newDataError })
@@ -64,6 +61,12 @@ app.get('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/restaurants/:id/delete', (req, res) => {
+  restaurantSeed.findById(req.params.id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
@@ -76,7 +79,6 @@ app.get('/search', (req, res) => {
       res.render('index', { restaurants: searchData, keyword: req.query.keyword })
     })
     .catch(error => console.log(error))
-
 })
 
 //POST method request---------------------------------------------------------------------------
@@ -101,4 +103,5 @@ app.post('/restaurants/:id/edit', (req, res) => {
       return res.redirect(`/restaurants/${req.params.id}/edit`) })
     .catch(error => res.redirect(`/restaurants/${req.params.id}/edit`) )
 })
+
 
