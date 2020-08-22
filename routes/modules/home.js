5942d0acef0596fc190d13cbb6e91af1dd06fe06
Corey <simon8888xsimon8888x@gmail.com>
2020-08-22 17:51:23 +0800

    refactor: remove sort/search route and put it in home route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,4 +11,36 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const userId = req.user._id
+  restaurantSeed.find({
+    userId,
+    $or: [{ name: { $regex: keyword, $options: 'i' } }, { category: { $regex: keyword, $options: 'i' } },
+    { name_en: { $regex: keyword, $options: 'i' } }]
+  })
+    .lean()
+    .then(restaurants => {
+      res.render('index', { restaurants, keyword: req.query.keyword })
+    })
+    .catch(error => console.log(error))
+})
+
+router.get('/sort', (req, res) => {
+  const userId = req.user._id
+  const keyword = req.query.keyword
+  const sort = {}
+  if (keyword === "A -> Z") sort.name = 1
+  else if (keyword === "Z -> A") sort.name = -1
+  else if (keyword === "類別") sort.category = 1
+  else sort.location = 1
+
+  restaurantSeed.find({ userId })
+    .lean()
+    .sort(sort)
+    .then(restaurants => res.render('index', { restaurants, sortKeyword: keyword }))
+    .catch(error => console.log(error))
+})
+
 module.exports = router
