a63c568384d02843b5bd7d8537e1ff446d2a2089
Corey <simon8888xsimon8888x@gmail.com>
2020-08-22 17:36:22 +0800

    fix: search function error

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -6,10 +6,11 @@ const restaurantSeed = require('../../models/restaurant.js')
 
 router.get('/', (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
-  const userId = req.user._id 
-  restaurantSeed.find({ userId,
-    $or: [{ name: keyword }, { category: keyword },
-    { name_en: { $regex: new RegExp("^" + keyword, "i") } }]
+  const userId = req.user._id
+  restaurantSeed.find({
+    userId,
+    $or: [{ name: { $regex: keyword, $options: 'i' } }, { category: { $regex: keyword, $options: 'i' } },
+    { name_en: { $regex: keyword, $options: 'i' } }]
   })
     .lean()
     .then(restaurants => {
