c1dbd2232fe0d75eb6ed0b691fa0aef741d1ce5c
Corey <simon8888xsimon8888x@gmail.com>
2020-08-23 04:49:50 +0800

    refactor: restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -8,6 +8,7 @@ const db = require('../../config/moogoose.js')
 const users = [{ name: 'user1', email: 'user1@example.com', password: '12345678' },
 { name: 'user2', email: 'user2@example.com', password: '12345678' }]
 
+
 db.once('open', () => {
   userSchema.findOne({ email: users[0].email })
     .then((user) => {
@@ -17,21 +18,16 @@ db.once('open', () => {
         .catch((error) => console.log(error))
     })
     .then(users => {
-      return Promise.all([createUserData(users[0], seedData[0]),
-      createUserData(users[0], seedData[1]),
-      createUserData(users[0], seedData[2]),
-      createUserData(users[1], seedData[3]),
-      createUserData(users[1], seedData[4]),
-      createUserData(users[1], seedData[5]),
-      ])
+      return Promise.all(Array.from({ length: 6 }, (_, i) => {
+        if (i > 2) return createUserData(users[1], seedData[i])
+        return createUserData(users[0], seedData[i])
+      }))
         .then(() => console.log('create all users data'))
         .catch((error) => console.log(error))
     })
     .then(() => process.exit())
     .catch((error) => console.log(error))
 })
-
-
 
 // function ///////////////////////////////////////////////////////////
 function createUser(user) {
@@ -43,18 +39,10 @@ function createUser(user) {
       })
       .then((hash) => {
         const { name, email } = user
-        return userSchema.create({
-          name,
-          email,
-          password: hash,
-        })
+        return userSchema.create({ name, email, password: hash })
       })
-      .then((user) => {
-        return resolve(user)
-      })
-      .catch((err) => {
-        return reject('user create fail')
-      })
+      .then((user) => resolve(user))
+      .catch((err) => reject('user create fail'))
   });
 }
 
